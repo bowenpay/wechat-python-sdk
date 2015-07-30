913d7b449e9f2246fca10ad6d0f107607de4e671
yijingping <jingping.yi@gmail.com>
2015-07-30 13:51:32 +0800

    add wechat-pay & get_js_config

--- wechat_sdk/basic.py
+++ wechat_sdk/basic.py
@@ -7,6 +7,8 @@ import json
 import cgi
 import urllib
 from StringIO import StringIO
+from random import Random
+import xmltodict
 
 from xml.dom import minidom
 
@@ -23,7 +25,7 @@ class WechatBasic(object):
     仅包含官方 API 中所包含的内容, 如需高级功能支持请移步 ext.py 中的 WechatExt 类
     """
     def __init__(self, token=None, appid=None, appsecret=None, partnerid=None,
-                 partnerkey=None, paysignkey=None, access_token=None, access_token_expires_at=None,
+                 partnerkey=None, paysignkey=None, notify_url='', access_token=None, access_token_expires_at=None,
                  jsapi_ticket=None, jsapi_ticket_expires_at=None, checkssl=False):
         """
         :param token: 微信 Token
@@ -32,6 +34,7 @@ class WechatBasic(object):
         :param partnerid: 财付通商户身份标识, 支付权限专用
         :param partnerkey: 财付通商户权限密钥 Key, 支付权限专用
         :param paysignkey: 商户签名密钥 Key, 支付权限专用
+        :param notify_url: 微信支付成功回调url, 支付权限专用
         :param access_token: 直接导入的 access_token 值, 该值需要在上一次该类实例化之后手动进行缓存并在此处传入, 如果不传入, 将会在需要时自动重新获取
         :param access_token_expires_at: 直接导入的 access_token 的过期日期，该值需要在上一次该类实例化之后手动进行缓存并在此处传入, 如果不传入, 将会在需要时自动重新获取
         :param jsapi_ticket: 直接导入的 jsapi_ticket 值, 该值需要在上一次该类实例化之后手动进行缓存并在此处传入, 如果不传入, 将会在需要时自动重新获取
@@ -47,6 +50,7 @@ class WechatBasic(object):
         self.__partnerid = partnerid
         self.__partnerkey = partnerkey
         self.__paysignkey = paysignkey
+        self.__notify_url = notify_url
 
         self.__access_token = access_token
         self.__access_token_expires_at = access_token_expires_at
@@ -954,6 +958,132 @@ class WechatBasic(object):
                 "data": unicode_data
             }
         )
+
+    def get_js_config(self, param):
+        ticket = self.jsapi_ticket
+        nonceStr = self.generate_random_string()
+        timestamp = self.get_timestamp()
+        signature = self.generate_jsapi_signature(timestamp, nonceStr, param["url"], ticket);
+        result = {
+            "debug": param.get("debug", False),
+            "appId": self.__appid,
+            "timestamp": timestamp,
+            "nonceStr": nonceStr,
+            "signature": signature,
+            "jsApiList": param["jsApiList"]
+        }
+        return result
+
+    ## 微信支付 start
+    def build_sign(self, params):
+        # 对所有传入参数按照字段名的 ASCII 码从小到大排序（字典序）
+        keys = params.keys()
+        keys.sort()
+
+        array = []
+        for key in keys:
+            # 值为空的参数不参与签名
+            if params[key] == None or params[key] == '':
+                continue
+            # sign不参与签名
+            if key == 'sign':
+                continue
+            array.append("%s=%s" % (key, params[key]))
+        # 使用 URL 键值对的格式拼接成字符串string1
+        string1 = "&".join(array)
+
+        # 在 string1 最后拼接上 key=Key(商户支付密钥)得到 stringSignTemp 字符串
+        stringSignTemp = string1 + '&key=' + self.__partnerkey
+
+        # 对 stringSignTemp 进行 md5 运算，再将得到的字符串所有字符转换为大写
+        m = hashlib.md5(stringSignTemp.encode('utf-8'))
+        return m.hexdigest().upper()
+
+    def build_unifiedorder(self, params):
+        base_params = {
+            'appid': self.__appid,
+            'mch_id': self.__partnerid,
+            'nonce_str': self.generate_random_string(),
+            'trade_type': 'JSAPI',
+            'body': params['body'],
+            'out_trade_no': params['out_trade_no'],
+            'total_fee': params['total_fee'],
+            'spbill_create_ip': params['spbill_create_ip'],
+            'notify_url': self.__notify_url,
+            'openid': params['openid']
+        }
+
+        base_params['sign'] = self.build_sign(base_params)
+        return self.dict_to_xml(base_params)
+
+    def get_timestamp(self):
+        return str(int(time.time()))
+
+    def generate_random_string(self, randomlength=32):
+        str = ''
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        length = len(chars) - 1
+        random = Random()
+        for i in range(randomlength):
+            str += chars[random.randint(0, length)]
+        return str
+
+    def dict_to_xml(params):
+        xml_elements = ["<xml>",]
+        for (k, v) in params.items():
+            if str(v).isdigit():
+                xml_elements.append('<%s>%s</%s>' % (k, v, k))
+            else:
+                xml_elements.append('<%s><![CDATA[%s]]></%s>' % (k, v, k))
+        xml_elements.append('</xml>')
+        return ''.join(xml_elements)
+
+    def build_form_by_prepay_id(self, prepay_id):
+        base_params = {
+            'appId': self.__appid,
+            'timeStamp': self.get_timestamp(),
+            'nonceStr': self.generate_random_string(),
+            'package': "prepay_id=%s" % prepay_id,
+            'signType': "MD5"
+        }
+        base_params['paySign'] = self.build_sign(base_params)
+        return base_params
+
+    def build_form_by_params(self, params):
+        headers = {'Content-Type': 'application/xml'}
+        xml = self.build_unifiedorder(params).encode('utf-8')
+        response = requests.post('https://api.mch.weixin.qq.com/pay/unifiedorder', data=xml, headers=headers)
+        response.encoding = 'utf-8'
+        response_dict = xmltodict.parse(response.text)['xml']
+        if response_dict['return_code'] == 'SUCCESS':
+            return self.build_form_by_prepay_id(response_dict['prepay_id'])
+
+    def notify_string_to_params(self, string):
+        params = {}
+        key_value_array = string.split('&')
+        for item in key_value_array:
+            key, value = item.split('=')
+            params[key] = value
+        return params
+
+    def verify_notify_string(self, string):
+        params = self.notify_xml_string_to_dict(string)
+
+        notify_sign = params['sign']
+        del params['sign']
+
+        if self.build_sign(params) == notify_sign:
+            return True
+        return False
+
+    def notify_xml_string_to_dict(self, string):
+        xml_data = xmltodict.parse(string)['xml']
+        params = {}
+        for k in xml_data:
+            params[k] = xml_data[k]
+        return params
+
+    ## 微信支付 end
 
     @property
     def access_token(self):
